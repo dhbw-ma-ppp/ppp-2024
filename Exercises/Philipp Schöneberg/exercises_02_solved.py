e5304908bc6efb042aec316536d41dec624723f3
Philipp <philipp.schoeneberg2004@gmx.de>
2024-10-24 17:17:46 +0200

    Improvements based on the given comments
    line 32:
    I changed the name of the function to a shorter and equally understandable version.
    
    line 43, 45, 69, 70-80:
    I moved the print statements in the function.
    
    line 58, 59:
    I changed the names of the lists in my function "string_sorter" to better understandable ones.

--- Exercises/Philipp Schöneberg/exercises_02_solved.py
+++ Exercises/Philipp Schöneberg/exercises_02_solved.py
@@ -29,8 +29,8 @@
 # print out which value is returned by your function for the following list:
 commands = [1, 12, 2, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 1, 9, 19, 1, 5, 19, 23, 1, 6, 23, 27, 1, 27, 10, 31, 1, 31, 5, 35, 2, 10, 35, 39, 1, 9, 39, 43, 1, 43, 5, 47, 1, 47, 6, 51, 2, 51, 6, 55, 1, 13, 55, 59, 2, 6, 59, 63, 1, 63, 5, 67, 2, 10, 67, 71, 1, 9, 71, 75, 1, 75, 13, 79, 1, 10, 79, 83, 2, 83, 13, 87, 1, 87, 6, 91, 1, 5, 91, 95, 2, 95, 9, 99, 1, 5, 99, 103, 1, 103, 6, 107, 2, 107, 13, 111, 1, 111, 10, 115, 2, 10, 115, 119, 1, 9, 119, 123, 1, 123, 9, 127, 1, 13, 127, 131, 2, 10, 131, 135, 1, 135, 5, 139, 1, 2, 139, 143, 1, 143, 5, 0, 99, 2, 0, 14, 0]
 
-def list_manipulator (lst):
-    for i, a in enumerate(lst[::4]):
+def lst_manipulator (lst):
+    for i, elem in enumerate(lst[::4]):
         match lst[i*4]:
             case 1:
                 lst[lst[i*4+3]] = lst[lst[i*4+1]]+lst[lst[i*4+2]]
@@ -40,9 +40,10 @@ def list_manipulator (lst):
                 break
             case _:
                 print("The given opcode is unvalid.")
+    print(lst[0])
     return lst[0]
 
-print(list_manipulator(commands))
+lst_manipulator(commands)
 
 ###########################################
 # Write a function that takes an arbitrary number of unnamed arguments
@@ -55,26 +56,20 @@ print(list_manipulator(commands))
 # examples and verify that the output for these examples is correct.
 
 def string_sorter (*args):
-    lst_0 = []
-    lst_1 = []
+    lst_numbers = []
+    lst_mono_character = []
     for i in args:
         try:
             float(i)
-            lst_0.append(i)
+            lst_numbers.append(i)
         except ValueError:
             pass
         if(len(i) == 1):
-            lst_1.append(i)
-    return lst_0, lst_1
+            lst_mono_character.append(i)
+    print(lst_numbers)
+    print(lst_mono_character)
+    return lst_numbers, lst_mono_character
 
-lst_numbers, lst_mono_character = string_sorter("1", "2.0", "22")
-print(lst_numbers)
-print(lst_mono_character)
-
-lst_numbers, lst_mono_character = string_sorter("1", "2.0", "22", ".", "x", "HalloWelt", "Hello World")
-print(lst_numbers)
-print(lst_mono_character)
-
-lst_numbers, lst_mono_character = string_sorter("1", "2.0", "22", ".", "x", "HalloWelt", "Hello World", " ", "")
-print(lst_numbers)
-print(lst_mono_character)
+string_sorter("1", "2.0", "22")
+string_sorter("1", "2.0", "22", ".", "x", "HalloWelt", "Hello World")
+string_sorter("1", "2.0", "22", ".", "x", "HalloWelt", "Hello World", " ", "")
