bc6ddc29b87a224b8112f8125ff0210f62b87aa7
LinusL2005 <linus05.la@gmail.com>
2024-10-29 18:21:50 +0100

    Update exercises_02.py

--- Exercises/exercises_02.py
+++ Exercises/exercises_02.py
@@ -39,4 +39,4 @@ commands = [1, 12, 2, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 1, 9, 19, 1, 5, 
 #   The second list should contain all strings which contain just one character.
 # Think of some good inputs to test this functionality, write down at least three
 # examples and verify that the output for these examples is correct.
- 
+-
