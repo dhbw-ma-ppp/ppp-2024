6498ddce27068f9ee9cb5be8263a7f65edd40dcb
Philipp <philipp.schoeneberg2004@gmx.de>
2024-10-31 15:29:52 +0100

    Applied the discussed changes
    - I added function descriptions and therefore an explanation which numbers are accepted in my function "string_sorter".
    - I changed my function "lst_manipulator" in form of using a while loop instead of a for loop.
    - I added extra variables for the indexes read by my function "lst_manipulator".
    - I added the raising of an error instead of a print statement when an error occurs in the functionality of my function "lst_manipulator".
    - I fixed the look of the code to Flake8 standards.
    - I added function tests.

--- Exercises/Philipp Schöneberg/exercises_02_solved.py
+++ Exercises/Philipp Schöneberg/exercises_02_solved.py
@@ -29,21 +29,60 @@
 # print out which value is returned by your function for the following list:
 commands = [1, 12, 2, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 1, 9, 19, 1, 5, 19, 23, 1, 6, 23, 27, 1, 27, 10, 31, 1, 31, 5, 35, 2, 10, 35, 39, 1, 9, 39, 43, 1, 43, 5, 47, 1, 47, 6, 51, 2, 51, 6, 55, 1, 13, 55, 59, 2, 6, 59, 63, 1, 63, 5, 67, 2, 10, 67, 71, 1, 9, 71, 75, 1, 75, 13, 79, 1, 10, 79, 83, 2, 83, 13, 87, 1, 87, 6, 91, 1, 5, 91, 95, 2, 95, 9, 99, 1, 5, 99, 103, 1, 103, 6, 107, 2, 107, 13, 111, 1, 111, 10, 115, 2, 10, 115, 119, 1, 9, 119, 123, 1, 123, 9, 127, 1, 13, 127, 131, 2, 10, 131, 135, 1, 135, 5, 139, 1, 2, 139, 143, 1, 143, 5, 0, 99, 2, 0, 14, 0]
 
-def lst_manipulator (lst):
-    for i, elem in enumerate(lst[::4]):
-        match lst[i*4]:
-            case 1:
-                lst[lst[i*4+3]] = lst[lst[i*4+1]]+lst[lst[i*4+2]]
-            case 2:
-                lst[lst[i*4+3]] = lst[lst[i*4+1]]*lst[lst[i*4+2]]
-            case 99:
-                break
-            case _:
-                print("The given opcode is unvalid.")
-    print(lst[0])
+
+def lst_manipulator(lst):
+    """
+    This function takes as input a list of integers and returns a single
+    integer number. The numbers passed as argument form the working memory of
+    a simulated computer. This computer will start by looking at the first
+    value in the list passed to the function. This value will contain an `
+    opcode`. Valid opcodes are 1, 2 or 99. Encountering any other value when
+    expecting an opcode or a lengthwise unfitting list will raise an
+    RuntimeError. The meaning of opcodes is as follows:
+    - 1 indicates addition. The function will read values from the next two#
+    positions of your working memory, add them, and store the result in the
+    third position of your working memory. The three numbers immediately after
+    your opcode indicate the memory locations to read (first two values) and
+    write (third value) respectively.
+    - 2 indicates multiplication. Otherwise the same rules apply as for opcode 1.
+    - 99 indicates halt. the program should stop after encountering the opcode 99.\n
+    After the program stops, the function should return the value in the first
+    location (address 0) of your working memory.
+    """
+    i = 0
+    while True:
+        try:
+            match lst[i]:
+                case 1:
+                    index_1 = lst[i+1]
+                    index_2 = lst[i+2]
+                    index_3 = lst[i+3]
+                    lst[index_3] = lst[index_1]+lst[index_2]
+                    i += 4
+                case 2:
+                    index_1 = lst[i+1]
+                    index_2 = lst[i+2]
+                    index_3 = lst[i+3]
+                    lst[index_3] = lst[index_1]*lst[index_2]
+                    i += 4
+                case 99:
+                    break
+                case _:
+                    raise RuntimeError("The given opcode is unvalid.")
+        except IndexError:
+            raise RuntimeError("The given commandlist is unvalid.")
     return lst[0]
 
-lst_manipulator(commands)
+
+def test_lst_manipulator():
+    assert lst_manipulator([1, 0, 0, 0, 99]) == 2
+    assert lst_manipulator([1, 1, 1, 4, 99, 5, 6, 0, 99]) == 30
+    print('Function "lst_manipulator" tested succesfully!')
+
+
+test_lst_manipulator()
+print(lst_manipulator(commands))
+
 
 ###########################################
 # Write a function that takes an arbitrary number of unnamed arguments
@@ -55,7 +94,16 @@ lst_manipulator(commands)
 # Think of some good inputs to test this functionality, write down at least three
 # examples and verify that the output for these examples is correct.
 
-def string_sorter (*args):
+
+def string_sorter(*args):
+    """
+    This function can take a variable number of arguments and will sort them
+    into two lists. The first list will contain all arguments that can be
+    interpreted as a number by Python's float type cast function. The second
+    list will contain all arguments with only one character. Arguments that
+    match the requirements of both lists will end up in both lists. The lists
+    are returned in the order explained.
+    """
     lst_numbers = []
     lst_mono_character = []
     for i in args:
@@ -64,12 +112,16 @@ def string_sorter (*args):
             lst_numbers.append(i)
         except ValueError:
             pass
-        if(len(i) == 1):
+        if len(i) == 1:
             lst_mono_character.append(i)
-    print(lst_numbers)
-    print(lst_mono_character)
     return lst_numbers, lst_mono_character
 
-string_sorter("1", "2.0", "22")
-string_sorter("1", "2.0", "22", ".", "x", "HalloWelt", "Hello World")
-string_sorter("1", "2.0", "22", ".", "x", "HalloWelt", "Hello World", " ", "")
+
+def test_string_sorter():
+    assert string_sorter("1", "2.0", "22", "-2", "3e2", "I", "II", "V") == (['1', '2.0', '22', '-2', '3e2'], ['1', 'I', 'V'])
+    assert string_sorter("1", "2.0", "22", "-2", "3e2", "I", "II", "V", ".", "x", "ä", "HalloWelt", "Hello World") == (['1', '2.0', '22', '-2', '3e2'], ['1', 'I', 'V', '.', 'x', 'ä'])
+    assert string_sorter("1", "2.0", "22", "-2", "3e2", "I", "II", "V", ".", "x", "ä", "HalloWelt", "Hello World", " ", "") == (['1', '2.0', '22', '-2', '3e2'], ['1', 'I', 'V', '.', 'x', 'ä', ' '])
+    print('Function "test_string_sorter" tested succesfully!')
+
+
+test_string_sorter()
