965202b83578ba2b60d6abada420f7fddf783bb3
atrejut <atreju.tauschinsky@gmx.de>
2024-11-13 20:37:32 +0100

    Unterlagen Vorlesung 2024-11-13

---
+++ Exercises/exercises_06.py
@@ -0,0 +1,27 @@
+
+# For this weeeks exercise you again need to add a feature to your existing simulated computer:
+
+# - The computer needs to implement memory much /larger/ than the set of initial commands.
+#   Any memory address not part of the initial commands can be assumed to be initialized to 0.
+#   Only positive addresses are valid, but any positive address can be both read and written.
+# - You need to support a new parameter mode, called 'relative mode', and denoted as mode 2 in 
+#   the 'mode' part of the instructions.
+#   Relative mode is similar to position mode (the first access mode you implemented). However, 
+#   parameters in relative mode count not from 0, but from a value called 'relative offset'. 
+#   When the computer is initialized, the relative offset is initialized to 0, and as long as it remains
+#   0 relative mode and position mode are identical.
+#   In general though parameters in relative mode address the memory location at 'relative offset + parameter value'.
+#   EXAMPLE: if the relative offset is 50, the mode is 2, and the value you read from memory is 7 you should 
+#     retrieve data from the memory address 57.
+#     Equally, if you read -7, you should retrieve data from the memory address 43.
+#   This applies to both read- and write operations (!)
+
+# - You need to implement a new opcode, opcode 9. opcode 9 adjusts the relative offset by the value of its only parameter.
+#   the offset increases by the value of the parameter (or decreases if the parameter value is negative).
+
+# Here is a short program to test your implementation:
+#   [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99] should output a copy of itself
+#   
+# The actual input is in a separate file, input_memory_01.txt under the `data` folder.
+# When asked for input provide the value 2. There should be a single output. Please include it in your PR.
+
