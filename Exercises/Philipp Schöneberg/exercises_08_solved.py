f36b95395dd3c3573dba1a24dc9e82d88e54ba3b
Philipp <philipp.schoeneberg2004@gmx.de>
2024-12-04 21:41:53 +0100

    Shortend my Code
    - Used an adjacency list instead of objects to represent the weighted graph as well as the nodes.
    - Applied the code to the matrix given in the file
    - My solution is 510

--- Exercises/Philipp Schöneberg/exercises_08_solved.py
+++ Exercises/Philipp Schöneberg/exercises_08_solved.py
@@ -1,7 +1,6 @@
 import numpy as np
-import numpy.typing as npt
-from typing import Generator
 from collections import defaultdict
+
 
 # - Solve the exercise described at https://adventofcode.com/2021/day/15 using the data at `data/exercise_cave.txt` as input. Prepare a PR as usual.
 #
@@ -13,156 +12,130 @@ from collections import defaultdict
 #   - Have some initial idea of what you want to achieve
 #   - No need to prepare any code yet. You will have the entire lecture on Friday to start working.
 
-cave_risklevels_list: npt.NDArray
-cave_risklevels_list = np.array([
-                                [1, 1, 6, 3, 7, 5, 1, 7, 4, 2],
-                                [1, 3, 8, 1, 3, 7, 3, 6, 7, 2],
-                                [2, 1, 3, 6, 5, 1, 1, 3, 2, 8],
-                                [3, 6, 9, 4, 9, 3, 1, 5, 6, 9],
-                                [7, 4, 6, 3, 4, 1, 7, 1, 1, 1],
-                                [1, 3, 1, 9, 1, 2, 8, 1, 3, 7],
-                                [1, 3, 5, 9, 9, 1, 2, 4, 2, 1],
-                                [3, 1, 2, 5, 4, 2, 1, 6, 3, 9],
-                                [1, 2, 9, 3, 1, 3, 8, 5, 2, 1],
-                                [2, 3, 1, 1, 9, 4, 4, 5, 8, 1]])
+risk_map_list: list[list[int]] = [[1, 1, 6, 3, 7, 5, 1, 7, 4, 2],
+                                  [1, 3, 8, 1, 3, 7, 3, 6, 7, 2],
+                                  [2, 1, 3, 6, 5, 1, 1, 3, 2, 8],
+                                  [3, 6, 9, 4, 9, 3, 1, 5, 6, 9],
+                                  [7, 4, 6, 3, 4, 1, 7, 1, 1, 1],
+                                  [1, 3, 1, 9, 1, 2, 8, 1, 3, 7],
+                                  [1, 3, 5, 9, 9, 1, 2, 4, 2, 1],
+                                  [3, 1, 2, 5, 4, 2, 1, 6, 3, 9],
+                                  [1, 2, 9, 3, 1, 3, 8, 5, 2, 1],
+                                  [2, 3, 1, 1, 9, 4, 4, 5, 8, 1]]
 
 
-class Node:
-    value: int
-
-    def __init__(self, value: int, node_top = None, node_bottom = None, node_left = None, node_right = None) -> None:
-        """
-        Creates a node with a maximum of four other nodes connected to it. The
-        node will also contain a value representing the weight of all links
-        going to it.
-        """
-        self.value = value
-        self.node_top: Node = node_top
-        self.node_bottom: Node = node_bottom
-        self.node_left: Node = node_left
-        self.node_right: Node = node_right
-        self.neighbours: list[Node] = []
-        if node_top:
-            self.neighbours.append(node_top)
-        if node_bottom:
-            self.neighbours.append(node_bottom)
-        if node_left:
-            self.neighbours.append(node_left)
-        if node_right:
-            self.neighbours.append(node_right)
-
-
-class weightedGraph:
-    nodes: list[list[Node]]
-
-    def __init__(self, lines: int, columns: int) -> None:
-        """
-        Creates an empty 2D array to hold any nodes added in the future.
-        """
-        self.nodes = np.empty((lines, columns), object)
-
-    def __iter__(self) -> Generator[int, None, None]:
-        """
-        Returns every node of the weightedGraph instance, going through every
-        row and within every row, through every column and so covering every
-        element.
-        """
-        for line in range(len(self.nodes)):
-            for column in range(len(self.nodes[line])):
-                yield self.nodes[line][column]
-
-    def addNode(self, line: int, column: int, node_value: int, has_top_neighbour: bool, has_left_neighbour: bool) -> None:
-        """
-        Adds a node to the weightedGraph instance in the order given by the 2D
-        array created by initialising.
-        """
-        if has_top_neighbour and has_left_neighbour:
-            newNode = Node(node_value, self.nodes[line-1][column], None, self.nodes[line][column-1], None)
-            self.nodes[line-1][column].node_bottom = newNode
-            self.nodes[line-1][column].neighbours.append(newNode)
-            self.nodes[line][column-1].node_right = newNode
-            self.nodes[line][column-1].neighbours.append(newNode)
-        elif has_top_neighbour:
-            newNode = Node(node_value, self.nodes[line-1][column], None, None, None)
-            self.nodes[line-1][column].node_bottom = newNode
-            self.nodes[line-1][column].neighbours.append(newNode)
-        elif has_left_neighbour:
-            newNode = Node(node_value, None, None, self.nodes[line][column-1], None)
-            self.nodes[line][column-1].node_right = newNode
-            self.nodes[line][column-1].neighbours.append(newNode)
-        else:
-            newNode = Node(node_value, None, None, None, None)
-        self.nodes[line][column] = newNode
-
-
-def dijkstra_search(graph: weightedGraph, aim: Node) -> tuple[int, list[Node]]:
+def read_file(input_file: str) -> list[list[int]]:
     """
-    Searches for a given node inside a given weightedGraph instance and
-    returns the distance given by the weights of the connections as well as a
-    list of nodes on the path from the root of the graph to the targeted node.
+    Creates and returns a list of integers from a given file.
     """
-    def search(nodes_to_go: list[Node], node: Node) -> list:
+    risk_map_list: list[list[int]] = []
+    with open(input_file, "r") as f:
+        for line in f:
+            line_list: list[int] = []
+            line = line.strip()
+            for char in line:
+                line_list.append(int(char))
+            risk_map_list.append(line_list)
+    return risk_map_list
+
+
+def create_adjacency_list(risk_map_list: list) -> dict[int, list[tuple]]:
+    """
+    Generates an adjacency list of a graph given as a list of list of integers.
+    Each node is herby connected to the one above and below it, as well as to
+    each node next to it.
+    """
+    risk_map_adjacency_list: dict[tuple[int, int], list[tuple]] = defaultdict(list)
+    lines = len(risk_map_list)
+    columns = len(risk_map_list[0])
+    for line in range(lines):
+        for column in range(columns):
+            node_number: int = line*columns+column
+            node_value: int = risk_map_list[line][column]
+            if line > 0:
+                # Has top-neighbour
+                risk_map_adjacency_list[(node_number, node_value)].append(((line-1)*columns+column, risk_map_list[line-1][column]))
+            if line < lines-1:
+                # Has bottom-neighbour
+                risk_map_adjacency_list[(node_number, node_value)].append(((line+1)*columns+column, risk_map_list[line+1][column]))
+            if column > 0:
+                # Has left-neighbour
+                risk_map_adjacency_list[(node_number, node_value)].append((line*columns+column-1, risk_map_list[line][column-1]))
+            if column < columns-1:
+                # Has right-neighbour
+                risk_map_adjacency_list[(node_number, node_value)].append((line*columns+column+1, risk_map_list[line][column+1]))
+    return risk_map_adjacency_list
+
+
+def dijkstra_search(risk_map_adjacency_list: dict[int, list[tuple]], start: tuple[int, int], aim: tuple[int, int]) -> tuple[int, list[int]]:
+    """
+    Searches for a given node within a given adjacency list of a weighted
+    graph and returns the distance given by the weights of the links, as well
+    as the list of nodes on the path from the starting node to the target node.
+    """
+    def search(risk_map_adjacency_list: dict[int, list[tuple]], nodes_to_go: list[int], node: tuple[int, int]) -> list[int]:
         """
         Visits the givin node and fetches the dist_to, edge_to and visited
         dictionaries, as well as the nodes_to_go list.
         """
         visited[node] = True
-        for elem in node.neighbours:
+        for elem in risk_map_adjacency_list[node]:
             if visited[elem] is False:
-                if dist_to[elem] > dist_to[node]+elem.value:
-                    dist_to[elem] = dist_to[node]+elem.value
+                if dist_to[elem] > dist_to[node]+elem[1]:
+                    dist_to[elem] = dist_to[node]+elem[1]
                     edge_to[elem] = node
                 nodes_to_go.append(elem)
         return nodes_to_go
 
-    def calculate_edges(edges: list[int], edge_to: dict[Node, Node], aim) -> list[int]:
+    def calculate_edges(edges: list[int], edge_to: dict[int, int], aim: tuple[int, int]) -> list[int]:
         """
-        Fills the edges list with every node from the root to the target.
+        Fills the edge list with the weight of each node on the way from the
+        start to the target node.
         """
         if edge_to[aim]:
             edges = calculate_edges(edges, edge_to, edge_to[aim])
-        edges.append(aim.value)
+        edges.append(aim[1])
         return edges
 
-    dist_to: dict[Node, int] = defaultdict(lambda: np.inf)
-    edge_to: dict[Node, Node] = defaultdict(lambda: None)
-    visited: dict[Node, bool] = defaultdict(bool)
-    nodes_to_go: list[Node] = []
-    nodes: list[Node] = []
+    dist_to: dict[tuple[int, int], int] = defaultdict(lambda: np.inf)
+    edge_to: dict[tuple[int, int], tuple[int, int]] = defaultdict(lambda: None)
+    visited: dict[tuple[int, int], bool] = defaultdict(bool)
+    nodes_to_go: list[tuple[int, int]] = []
     edges: list[int] = []
-    for elem in graph:
-        nodes.append(elem)
-    dist_to[nodes[0]] = 0
-    search(nodes_to_go, nodes[0])
+
+    dist_to[start] = 0
+    nodes_to_go = search(risk_map_adjacency_list, nodes_to_go, start)
     while nodes_to_go:
-        nodes_to_go = search(nodes_to_go, nodes_to_go.pop(0))
+        node = nodes_to_go.pop(0)
+        if visited[node] is False:
+            nodes_to_go = search(risk_map_adjacency_list, nodes_to_go, node)
+        if visited[aim] is True:
+            break
     edges = calculate_edges(edges, edge_to, aim)
     return dist_to[aim], edges
 
 
-def create_weighted_graph(cave_risklevels_list: list[int]) -> weightedGraph:
+def test():
     """
-    Creates a weightedGraph instance of the same size as a given 2D list. Each
-    node is connected to the one above and below it, as well as to each node
-    next to it.
-
-    Attention! This list must be able to be converted to a numpy array.
+    Tests the functionality of the programme.
     """
-    lines = len(cave_risklevels_list)
-    columns = len(cave_risklevels_list[0])
-    cave_risklevels__weighted_graph = weightedGraph(lines, columns)
-    for line in range(lines):
-        for column, elem in enumerate(cave_risklevels_list[line]):
-            has_left_neighbour: bool = False
-            has_top_neighbour: bool = False
-            if line > 0:
-                has_top_neighbour = True
-            if column > 0:
-                has_left_neighbour = True
-            cave_risklevels__weighted_graph.addNode(line, column, elem, has_top_neighbour, has_left_neighbour)
-    return cave_risklevels__weighted_graph
+    lines = len(risk_map_list)
+    columns = len(risk_map_list[0])
+    risk_map_adjacency_list = create_adjacency_list(risk_map_list)
+    assert 40, [1, 1, 2, 1, 3, 6, 5, 1, 1, 1, 5, 1, 1, 3, 2, 3, 2, 1, 1] == dijkstra_search(risk_map_adjacency_list, (0, risk_map_list[0][0]), (len(risk_map_adjacency_list)-1, risk_map_list[lines-1][columns-1]))
+    print("The test was successful!")
 
 
-cave_risklevels_weighted_graph = create_weighted_graph(cave_risklevels_list)
-distance, edges = dijkstra_search(cave_risklevels_weighted_graph, cave_risklevels_weighted_graph.nodes[len(cave_risklevels_list)-1][len(cave_risklevels_list[0])-1])
-print(f"The best way contains a risk of {distance} and has a risk path as follows: {edges}.")
+test()
+
+risk_map_list = read_file("exercise_cave.txt")
+
+distance: int
+edges: list[int]
+lines: int = len(risk_map_list)
+columns: int = len(risk_map_list[0])
+risk_map_adjacency_list: dict[int, list[tuple]]
+
+risk_map_adjacency_list = create_adjacency_list(risk_map_list)
+distance, edges = dijkstra_search(risk_map_adjacency_list, (0, risk_map_list[0][0]), (len(risk_map_adjacency_list)-1, risk_map_list[lines-1][columns-1]))
+print(f"The best path contains a risk of {distance} and has a risk path as follows:\n{edges}.")
